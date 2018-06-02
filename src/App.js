72ed03e312d12bd28633c0410dbbc7bef9b39265
RageBill <kelvin@firstcodeacademy.com>
2018-06-02 17:38:25 +0800

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import Title from './components/Title/Title';
 import PokemonGenerator from './components/PokemonGenerator/PokemonGenerator';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
